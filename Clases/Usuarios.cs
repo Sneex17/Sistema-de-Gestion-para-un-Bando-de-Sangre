8f485ac087b36847722115734aea1a5d65ed2f89
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2025-09-08 11:51:34 -0400

    Se agrego la carpeta de clases

---
+++ Clases/Usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gestión_para_un__Bando_de_Sangre.Clases
+{
+    public class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public string usuario {  get; set; }
+        public string pass { get; set; }
+    }
+}
